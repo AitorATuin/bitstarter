e280b039eeb78143ecbcedae1f8afe566a34fe3a
Aitor P. Iturri <aitor.iturri@gmail.com>
2013-07-06 01:04:06 +0200

    HW3 - Part1

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+function renderIndex(file)  {
+    return fs.readFileSync(file);
+}
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    response.send(renderIndex("index.html").toString());
 });
 
 var port = process.env.PORT || 5000;
